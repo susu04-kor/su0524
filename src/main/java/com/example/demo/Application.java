bfbaeb491c714cf20afcf102c101ae03496fad62
susu04-kor <suin0304_sw@google.com>
2020-05-24 18:58:20 +0900

    수인) 호호

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	//휴휴
+	//휴휴하하하
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
